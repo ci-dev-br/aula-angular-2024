f71b4bb8e577a21c64c59d2549f8a284ea61d248
Paulinho <paulo.rettamozo@nsinova.com.br>
2024-05-22 17:37:24 -0300

    ++

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import { Interceptor } from './interceptor';
 import { ApiModule } from 'src/api/api.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material';
 registerLocaleData(localePt);
 
 @NgModule({
@@ -28,6 +30,8 @@ registerLocaleData(localePt);
     FormsModule,
     HttpClientModule,
     ApiModule.forRoot({rootUrl: 'http://10.250.250.104:3000'}),
+    BrowserAnimationsModule,
+    MatNativeDateModule,
   ],
   exports: [
 
--- src/app/navegacao/profile/profile.module.ts
+++ src/app/navegacao/profile/profile.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 
 @NgModule({
@@ -12,6 +13,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ProfileRoutingModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
   ]
 })
 export class ProfileModule { }
